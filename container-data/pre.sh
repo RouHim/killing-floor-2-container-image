b00fd1b5bb8fe0e81710aa7ba0840e6bf648fd38
Rouven Himmelstein <rouven@himmelstein.info>
2024-04-14 11:32:45 +0200

    feat: use envini

--- container-data/pre.sh
+++ container-data/pre.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+# Replace all '\' with '/' in "/data/KFGame/Config/LinuxServer-KFEngine.ini"
+sed -i 's/\\/\//g' "/data/KFGame/Config/LinuxServer-KFEngine.ini"
+
 # Apply the env to ini mapping
-#"$USER_HOME"/envini "$USER_HOME"/envini_mapping.ini
+"$USER_HOME"/envini "$USER_HOME"/envini_mapping.ini
